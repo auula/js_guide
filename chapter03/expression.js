a5861a3b02efaed8f3e6638e2e771256f4f0fbbb
Leon Ding <ding@ibyte.me>
2023-05-02 00:35:08 +0800

    add: expression.

--- chapter03/expression.js
+++ chapter03/expression.js
@@ -24,9 +24,7 @@ function bubble(arr) {
 	for (let i = 0;i < arr.length - 1;i++) {
 		for (let j = 0; j < arr.length - 1 - i;j++) {
 			if (arr[j] > arr[j+1]) {
-				let temp = arr[j+1];
-				arr[j+1] = arr[j];
-				arr[j] = temp;
+				[arr[j+1],arr[j]] = [arr[j],arr[j+1]];
 			}
 		}
 	}
