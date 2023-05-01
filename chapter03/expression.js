7e4ef7dc37536dfdc4139c2deb918834f5572a85
Leon Ding <ding@ibyte.me>
2023-05-01 21:40:22 +0800

    add: expression.

---
+++ chapter03/expression.js
@@ -0,0 +1,16 @@
+// js 中的表达式和操作符
+// 表达式是又多个操作符组成的，可以是变量也可以运算符
+
+let num = 1.23;
+let str = "hello";
+let b = 0 === 0;
+
+console.log(num,str,b);
+
+let p = {
+	name:"Tom",
+	age:23,
+};
+let arr = [1+2,0===0,p];
+
+console.log(arr);
