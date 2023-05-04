480b8c26d6a122d95b376594da8ba873627678ac
Leon Ding <ding@ibyte.me>
2023-05-04 22:08:42 +0800

    add: while expression.

--- chapter04/while.js
+++ chapter04/while.js
@@ -2,6 +2,7 @@
 
 let n = 0;
 
+// 先判断循环条件再执行	
 while (n < 10) {
 	n += 1;
 }
@@ -14,6 +15,7 @@ function printArray(arr) {
 	if (len === 0) {
 		console.log("empty array.");
 	} else {
+		// 先执行一次再判断是否继续循环条件
 		do {
 			console.log(arr[i]);
 		} while (++i < len);
