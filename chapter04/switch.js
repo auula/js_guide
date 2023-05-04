1b7d5fd8ccdb1465a2e0582d56ab7917fcb68189
Leon Ding <ding@ibyte.me>
2023-05-04 22:00:41 +0800

    add: switch expression.

---
+++ chapter04/switch.js
@@ -0,0 +1,20 @@
+// js 中的 switch 语句，最好不要中 case 中写数值表达式
+
+function convert(x) {
+	switch(x) {
+		case "number":
+			return x.toString();
+		case "string":
+			return '"' + x + '"';
+		default:
+			return String(x);
+	}
+}
+
+let s = {
+	name: "Leon",
+	age: 23,
+	skill: ["Java","JavaScript","Go","Rust"],
+}
+
+console.log(convert(s).toString())
