413f4bbc5b3841ddb40ad1d4e443dc6d6b60afe4
Leon Ding <ding@ibyte.me>
2023-05-06 18:57:17 +0800

    add: object.

--- chapter05/object.js
+++ chapter05/object.js
@@ -14,3 +14,17 @@ let pointer = {
 };
 
 console.log(pointer);
+
+// 通过内置的 Object 类型的方法创建
+// 此种方式创建的有原型属性： Object.prototype
+let o1 = Object.create({x:1,y:2});
+
+console.log(o1.x + o1.y);
+
+// 创建 null 的对象，不会继承任何东西，连 toString() 也没有用
+let o2 = Object.create(null);
+
+console.log(o2);
+
+// 具有 Object.prototype 这样的属性
+let o3 = Object.create(Object.prototype);
