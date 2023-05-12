1d922b957ff3e978760fdc53b44b8f9045016eb6
Leon Ding <ding@ibyte.me>
2023-05-12 16:32:47 +0800

    add: arrays.

---
+++ chapter06/array.js
@@ -0,0 +1,13 @@
+// js 中的数值对象使用，js 中的数值和传统编程语言不一样，类型不同的元素可以放到一起
+
+let arr1 = [1,2,3,4,5,6,];
+
+// length 为 6 最后一个逗号不算
+console.log(arr1.length);
+console.log(arr1);
+
+// js 中的数组可以不同类型的，并且支持扩展操作符号
+let arr2 = [...arr1,["array",{x:123.13,y:3.1415}]];
+
+console.log(arr2.length);
+console.log(arr2);
