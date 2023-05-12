352f112834f78dc3493b2ce7168abe755385e8c6
Leon Ding <ding@ibyte.me>
2023-05-12 21:03:49 +0800

    add: arrays.

--- chapter06/array_from.js
+++ chapter06/array_from.js
@@ -5,3 +5,15 @@ let a = [1,234,13123221312,"string"];
 let arr = Array.from(a);
 
 console.log(arr);
+
+// 可以传入第二个参数，传入一个函数
+
+function add(x) {
+	return x + x;
+}
+
+let arr2 = [1,23,44,55,100];
+
+let arr3 = Array.from(arr2,add);
+
+console.log(arr3);
