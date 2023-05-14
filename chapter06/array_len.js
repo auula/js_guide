307d2b67486435c85b5c720472fe0e799f5b1f9b
Leon Ding <deen.job@qq.com>
2023-05-14 14:14:45 +0800

    add: array length.

--- chapter06/array_len.js
+++ chapter06/array_len.js
@@ -27,3 +27,20 @@ arr2['1000'] = 1000;
 
 console.log(arr2)
 
+// 从头部插入
+let arr3 = [];
+
+// 这样插入元素会带来一个问题，所以元素会往后移动一位 O(n)
+arr3.unshift(1);
+arr3.unshift(2);
+arr3.unshift(3);
+arr3.unshift(4);
+
+// 从头部返回
+console.log(arr3.pop())
+
+delete arr3[1];
+
+console.log(1 in arr3)
+
+console.log(arr3);
