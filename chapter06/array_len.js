b548bf2775fc38970b13b87f94c16d9806d6ea51
Leon Ding <deen.job@qq.com>
2023-05-14 13:27:39 +0800

    add: array length.

--- chapter06/array_len.js
+++ chapter06/array_len.js
@@ -9,4 +9,21 @@ console.log(arr.length);
 
 console.log(arr);
 
+// js 中的如果认为的修改了 length 会导致数组的元素丢失，能访问到的元素只能到 length 中为止
+
+let arr2 = new Array(10);
+
+console.log(arr2)
+console.log(arr2.length)
+
+// 此处修改了数组的长度
+arr2.length = 5
+
+console.log(arr2)
+console.log(arr2.length)
+
+// 添加了一个元素，length 已经到达了 1000 + 1
+arr2['1000'] = 1000;
+
+console.log(arr2)
 
