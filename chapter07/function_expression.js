a1b53eab91d88e3bc93c25da94a6120617b4e487
Leon Ding <deen.job@qq.com>
2023-05-17 00:20:40 +0800

    add: function.

---
+++ chapter07/function_expression.js
@@ -0,0 +1,16 @@
+// js 中的函数表达式，js 中函数变量
+
+// 把函数赋值给一个变量
+const square = function(x) {x * x};
+
+// 用自定义名称的函数变量
+const f = function fact(x) { if (x <= 1) return 1; else return x * fact(x - 1)};
+console.log(f(10));
+
+// 函数也可以作为参数传入另外一个函数中使用
+let s = [3,2,1].sort(function(a,b){return a - b});
+console.log(s);
+
+// 立即调用一个函数表达式
+let t = function(x){return x * x}(10);
+console.log(t);
