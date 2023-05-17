8954293ea4fa863d2e89256d6c0b6d531781eab8
Leon Ding <deen.job@qq.com>
2023-05-17 15:24:21 +0800

    add: object method.

--- chapter07/object_method.js
+++ chapter07/object_method.js
@@ -20,3 +20,23 @@ let calculator = {
 calculator.add();
 // 计算结果
 console.log(calculator.result);
+
+// js 对象方法级联调用
+let s = {
+    x: function(x) {
+      this.x = x;
+      return this;
+    },
+    y: function(y) {
+      this.y = y;
+      return this;
+    },
+    t: function(t) {
+      this.t = t;
+      return this;
+    },
+  };
+  
+  s.x(100).y(200).t(300);
+  
+  console.log(s.x, s.y, s.t);
