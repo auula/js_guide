855e080a26c821926ca18a13c77daa5e90aaa257
Leon Ding <deen.job@qq.com>
2023-05-18 17:56:51 +0800

    add: object method.

--- chapter07/function_closure.js
+++ chapter07/function_closure.js
@@ -1,4 +1,5 @@
 // js 中的闭包使用，函数可以返回一个内部定义的函数
+// 闭包可以让调用者无法操作函数内部的变量，只能通过特定函数来操作
 
 let uniqueIntger = (function(){
     let counter = 0;
@@ -12,6 +13,9 @@ console.log(uniqueIntger());
 console.log(uniqueIntger());
 console.log(uniqueIntger());
 
+
+// 此种方式效果可以实现类似于对象 get set 效果
+
 function counter() {
     let n = 0;
     return {
@@ -19,6 +23,7 @@ function counter() {
         reset: () => { n = 0 },
     };
 }
+
 
 let c = counter(),d = counter();
 c.count();
