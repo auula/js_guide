9a829905121c11b8ffad15e3d89626f73b8f3bd0
Leon Ding <deen.job@qq.com>
2023-05-18 23:07:09 +0800

    add: object function bind.

---
+++ chapter07/function.js
@@ -0,0 +1,14 @@
+// js 中的函数对象和函数属性和函数方法
+// 在 js 中函数也是一种特殊的对象，有自有的属性和方法
+
+function fn(y) {
+    return this.x + y;
+}
+
+let obj = {x:1};
+
+// 会把 fn 绑定到 obj 上，并且返回一个新的函数
+let g = fn.bind(obj);
+
+console.log(g(10));
+
