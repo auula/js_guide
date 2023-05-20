d984f00ec62b67c4690969b043350f00c96e8c86
Leon Ding <deen.job@qq.com>
2023-05-20 12:03:39 +0800

    add: object class.

--- chapter08/class.js
+++ chapter08/class.js
@@ -1,1 +1,18 @@
 // js 中的对象的使用
+
+function range(from,to) {
+    let r = Object.create(range.methods);
+    r.from = from;
+    r.to = to;
+    return r;
+}
+
+range.methods = {
+    includes(x) {return this.from <= x && x <= this.to;},
+
+    *[Symbol.iterator]() {
+        for(let x = Math.ceil(this.from);x <= this.to;x++) yield x;
+    },
+
+    toString() {return "(" + this.from + "..." + this.to + ")";}
+}
