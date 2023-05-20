fde5df0a50bfb04575aeb6bdce3a2af47c13ce15
Leon Ding <deen.job@qq.com>
2023-05-20 12:29:59 +0800

    add: object class.

--- chapter08/class.js
+++ chapter08/class.js
@@ -1,5 +1,6 @@
 // js 中的对象的使用，早期的版本创建 js 对象方式
 
+// 类的构造函数
 function range(from,to) {
     let r = Object.create(range.methods);
     r.from = from;
@@ -7,6 +8,7 @@ function range(from,to) {
     return r;
 }
 
+// methods 为类添加方法
 range.methods = {
     // 检查某个值是否在范围内
     includes(x) {return this.from <= x && x <= this.to;},
