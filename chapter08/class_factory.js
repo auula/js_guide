5c1a6009a1c77d327843172e86c8573e03a93308
Leon Ding <deen.job@qq.com>
2023-05-21 15:43:52 +0800

    add: object class factory.

--- chapter08/class_factory.js
+++ chapter08/class_factory.js
@@ -1,6 +1,6 @@
 // js 中的对象工程函数，
 // 上一个例子使用的 range 函数创建，
-// 只能通过 rnage 函数调用创建对象实例。
+// 只能通过 range 函数调用创建对象实例。
 
 function Range(from,to) {
     this.from = from;
@@ -23,6 +23,11 @@ Range.prototype = {
     }
 }
 
+
+// js 中如果一个函数默认是以大写字母开头，
+// 运行时则会认为是构造函数，会主动初始化 this 关键字，
+// 不像之前的版本要使用 Object.create() 函数来创建
+// 此种构造函数必须 new 关键字来创建
 let r = new Range(1,10);
 
 console.log(r.includes(2));
