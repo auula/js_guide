0f717f15ea179743d99cdaacdf9492c87c479eb4
Leon Ding <deen.job@qq.com>
2023-05-22 20:55:27 +0800

    add: object class factory.

---
+++ chapter08/class_target.js
@@ -0,0 +1,13 @@
+// js 中传统构造函数，中属性 new.target 使用
+
+function Person(name) {
+    console.log(new.target);
+  }
+  
+  function Student(name) {
+    Person.call(this, name);
+  }
+  
+  var person1 = new Person("John"); // 输出 Person
+  var student1 = new Student("Alice"); // 输出 Student
+  
