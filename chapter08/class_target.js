bced670304c2aa77b493ac5219ebcbd68c353357
Leon Ding <deen.job@qq.com>
2023-05-23 16:44:01 +0800

    add: object new target.

--- chapter08/class_target.js
+++ chapter08/class_target.js
@@ -3,6 +3,7 @@
 
 // https://www.bookstack.cn/read/es6-3rd/spilt.6.docs-class.md
 function Foo() {
+    // 必须是通过 new 关键字调用次函数才能正常运行
     if (!new.target) throw "Foo() must be called with new";
     console.log("Foo instantiated with new");
 }
@@ -12,6 +13,7 @@ new Foo(); // logs "Foo instantiated with new"
 
 class A {
     constructor() {
+        // 返回都是当前实例化的类名称
         console.log(new.target.name);
     }
 }
