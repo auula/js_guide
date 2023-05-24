8fabf9ffcb0bd7dc621b5de8b4ce77eccbde5f69
Leon Ding <deen.job@qq.com>
2023-05-24 17:37:22 +0800

    add: object class static constructor.

---
+++ chapter08/class_static.js
@@ -0,0 +1,24 @@
+// js 中类型的静态方法
+
+
+class Person {
+
+    firstName = "";
+    lastName = "";
+
+    constructor(fname,lname) {
+        this.firstName = fname;
+        this.lastName  = lname;
+    }
+
+    static create([fn = "Leon",ln = "Ding"]) {
+        return new Person(fn,ln);
+    }
+}
+
+let name = ["Leon",,] ?? "Leon Ding";
+
+// 静态构造方法
+let p = Person.create(name);
+
+console.log(p);
