cd2790e20bb4c8b569af378820b62e71d5c4d4bb
Leon Ding <deen.job@qq.com>
2023-05-25 19:46:20 +0800

    add: class new private.

--- chapter08/class_static.js
+++ chapter08/class_static.js
@@ -11,6 +11,8 @@ class Person {
         this.lastName  = lname;
     }
 
+    // 静态属性构造函数，使用 static 关键字声明的属性为静态
+    // 非 static 声明的属性，属于对象实例属性
     static create([fn = "Leon",ln = "Ding"]) {
         return new Person(fn,ln);
     }
