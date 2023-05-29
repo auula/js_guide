cad9baf1032b088461d722c08398e10df430cbf0
Leon Ding <deen.job@qq.com>
2023-05-29 20:44:33 +0800

    add: class abstract.

--- chapter08/abstractSet.js
+++ chapter08/abstractSet.js
@@ -5,16 +5,45 @@
 // 抽象类可以作为一组子类的父类
 // 抽象类型可以定义子类的部分功能实现让类型共享
 
-class abstratSet {
+class AbstractSet {
     // 不允许直接初始化创建
     constructor() {
-        if (new.target) {
-            throw new TypeError("abstractSet not initialized, need to use extends subclasses to implement.");
+        if (new.target === AbstractSet) {
+            throw new TypeError("AbstractSet not initialized, need to use extends subclasses to implement.");
         }
     }
     // 公共的抽象方法
-    has(x) {throw new Error("abstractSet method.")}
+    has(x) {throw new Error("AbstractSet method.")}
 }
 
 
-let aset = new abstratSet();
+let aset = new AbstractSet();
+
+class MySet extends AbstractSet  {
+    // 私有的 set 成员
+    #set = null;
+
+    constructor() {
+        super();
+        this.#set = new Set();
+    }
+
+    has(x) {
+        return this.#set.has(x);
+    }
+
+    add(v) {
+        this.#set.add(v);
+    }
+}
+
+let ms = new MySet();
+
+ms.add(1);
+ms.add(2);
+ms.add(3);
+ms.add(4);
+
+console.log(ms.has(4));
+console.log(ms.has(5));
+
