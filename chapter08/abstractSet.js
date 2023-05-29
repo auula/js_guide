4037c7b3820249eacec0e15f0c9b46260a4b8422
Leon Ding <deen.job@qq.com>
2023-05-29 19:27:37 +0800

    add: class abstract.

--- chapter08/abstractSet.js
+++ chapter08/abstractSet.js
@@ -6,5 +6,15 @@
 // 抽象类型可以定义子类的部分功能实现让类型共享
 
 class abstratSet {
+    // 不允许直接初始化创建
+    constructor() {
+        if (new.target) {
+            throw new TypeError("abstractSet not initialized, need to use extends subclasses to implement.");
+        }
+    }
+    // 公共的抽象方法
     has(x) {throw new Error("abstractSet method.")}
 }
+
+
+let aset = new abstratSet();
