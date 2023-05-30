3e72816c0964d61e9b6a6f467c6b078d0a89b42d
Leon Ding <deen.job@qq.com>
2023-05-30 20:40:20 +0800

    add: node.js module.

---
+++ chapter09/module_nodejs.js
@@ -0,0 +1,15 @@
+// js 可以在浏览器上运行默认的操作对象上 Window
+// 前面的代码适用早期 es 版本
+
+// js 也可以在 node.js 运行环境上执行
+// nodejs 环境中导出导入模块，可以使用内置的 module.exports 全局属性
+
+// 默认上空的
+console.log(module.exports);
+
+exports.sayhi = () => {console.log("Hello Node.JS")};
+
+// Hello Node.JS
+exports.sayhi();
+
+
