f574118e8d300d2a653d42491726ab9b90a22343
Leon Ding <deen.job@qq.com>
2023-05-31 23:02:01 +0800

    add: es6 js module.

---
+++ chapter09/module_es6.js
@@ -0,0 +1,20 @@
+// js 中的 es6 原生支持模块导入导出
+
+const PI = Math.PI;
+
+function desgreesToRadians(d) {
+    return d * PI / 180;
+};
+
+class Circle {
+    // 构造函数
+    constructor(r) {
+        this.r = r;
+    }
+    area() {
+        return PI * this.r * this.r;
+    }
+}
+
+// 批量导出模块内部的变量和函数，看着像是对象，其实为简写。
+export {Circle,desgreesToRadians,PI}
