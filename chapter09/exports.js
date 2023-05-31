c44fe1c33a3ecd2d0f139ea5ee069ed832de0c15
Leon Ding <deen.job@qq.com>
2023-05-31 18:55:21 +0800

    add: node.js module.

--- chapter09/exports.js
+++ chapter09/exports.js
@@ -1,15 +1,18 @@
-// js 在 node.js 中到处模块教程
+// js 中到处函数到外部使用
 
-// 要导出的模块
-const exports = (function () {
+// 要导出的函数或者对象
+const math = (function () {
     function add(x, y) {
         return x + y;
     };
+
     function sub(x, y) {
         return x - y;
     };
 
     return { add, sub };
+
 }());
 
-
+// 导出这个模块
+module.exports = math;
