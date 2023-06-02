885e5c1121906df619f2b392da671a48acad9a17
Leon Ding <deen.job@qq.com>
2023-06-03 00:19:33 +0800

    add: es6 js module.

--- chapter09/export_default.js
+++ chapter09/export_default.js
@@ -1,7 +1,32 @@
 // 使用 export default 关键字可以将一个模块标记为默认导出，
 // 一个模块只能有一个默认导出可以在其他文件中使用，
-// import 关键字引入时，可以自定义导入名称。
+// import 关键字引入时，不需要解构或者指定导入的模块。
 
 
 // console.log(import.meta.url);
+
+export default class Person {
+
+    #name = "";
+    #age = 1;
+
+    // 默认的构造器
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    get name() {
+        return this.name;
+    };
+
+    get age() {
+        this.name = age;
+    }
+
+    toString() {
+        return "Hi! i am ${this.#name} , age is ${this.#age} .";
+    }
+
+}
 
---
+++ chapter09/import_default.js
@@ -0,0 +1,10 @@
+// export default：export default 用于导出一个默认的值或实体,
+// 每个模块只能有一个默认导出，默认导出的变量、函数或类在导入时可以使用不同的名称来引用。
+// 默认导出的语法如下所示：export default expression;
+// 这里的 expression 可以是任何有效的 JavaScript 表达式。
+
+import Person from './export_default.js';
+
+const person = new Person('John', 25);
+
+console.log(person);
