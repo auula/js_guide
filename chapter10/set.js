bd55b1e21ee25c01393c5a797eb653895ba17ac7
Leon Ding <deen.job@qq.com>
2023-06-05 23:38:21 +0800

    add: es6 standard library.

--- chapter10/set.js
+++ chapter10/set.js
@@ -27,3 +27,53 @@ console.log(s.size);
 s.clear()
 
 console.log(s);
+
+
+// 添加相同元素测试，元素值可以是相同的，但是并不代表它们是相等的
+
+s.add([1,2,3]);
+
+s.add([1,2,3]);
+
+// Set(2) { [ 1, 2, 3 ], [ 1, 2, 3 ] }
+console.log(s);
+
+// 这里是无法删除的，要删除必须使用引用的方式进行删除
+console.log(s.delete([1,2,3]));
+
+console.log(s);
+
+let a = Array.of(1,2,3,4,5);
+let b = Array.of(1,2,3,4,5);
+
+console.log(a === b);
+
+console.log(arraysAreEqual(a,b));
+
+s.add(a);
+
+s.add(b);
+
+console.log(s);
+
+// 使用引用来删除相同的数组元素
+s.delete(a);
+
+console.log(s);
+
+// 深度比较
+function arraysAreEqual(arr1, arr2) {
+    // 检查数组长度是否相等
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+  
+    // 逐个比较数组元素
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
