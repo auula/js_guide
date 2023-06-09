c217bfe7e5b874194df94659cc65426e0769d102
Leon Ding <deen.job@qq.com>
2023-06-09 20:39:34 +0800

    add: string & json example.

--- chapter10/json.js
+++ chapter10/json.js
@@ -22,3 +22,22 @@ with(person) {
     console.log(...skill);
     console.log(isMan);
 }
+
+
+// JSON 提供的 API 可以实现对象之间深度复制
+function deepcopy(src) {
+    return JSON.parse(JSON.stringify(src));
+}
+
+// 深度复制教程
+let dist = deepcopy(person);
+
+// 比较一下说明是两个不同地址，说明深度复制成功
+console.log(dist === person);
+
+with(dist) {
+    console.log(name);
+    console.log(age);
+    console.log(...skill);
+    console.log(isMan);
+}
