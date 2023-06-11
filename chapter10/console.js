4bac251fbd9fa3f03c0b993747fca12753347794
Leon Ding <deen.job@qq.com>
2023-06-11 22:55:19 +0800

    add: console example.

--- chapter10/console.js
+++ chapter10/console.js
@@ -14,4 +14,22 @@ console.error("this error std err out messages.");
 
 console.info("this info std err out messages.");
 
-console.debug("this debug std err out messages.");
+console.debug("this debug std err gout messages.");
+
+// 断言函数
+const array = [1, 2, 3];
+
+// 条件为 true，没有输出错误消息
+console.assert(array.length === 3, "数组长度不等于 3");
+
+const data = [
+    { name: 'John', age: 30, city: 'New York' },
+    { name: 'Jane', age: 25, city: 'San Francisco' },
+    { name: 'Bob', age: 35, city: 'Chicago' }
+];
+
+// 以表格的方式输出数据，标准输出
+console.table(data);
+
+// trace 和 log 一样，但是和会输出调用栈信息
+console.trace(data);
