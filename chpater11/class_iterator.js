ca145c7a77227791d21f474d353082fc90e6db8e
Leon Ding <deen.job@qq.com>
2023-06-12 18:01:35 +0800

    add: iterator example.

--- chpater11/class_iterator.js
+++ chpater11/class_iterator.js
@@ -2,7 +2,7 @@
 
 class Range {
 
-    constructor(from,to) {
+    constructor(from, to) {
         this.from = from;
         this.to = to;
     }
@@ -13,6 +13,20 @@ class Range {
 
     [Symbol.iterator]() {
         let next = Math.ceil(this.from);
+        let last = this.to;
+        return {
+            // 闭包导出
+            next() {
+                // 闭包导出
+                return (next <= last)
+                    ? { value: next++ }
+                    : { done: true };
+            },
+        }
     }
+};
 
+
+for (const x of new Range(1, 10)) {
+    console.log(x);
 }
