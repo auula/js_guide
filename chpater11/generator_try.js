8f639f388f98d03bf7e0780c4f5f539e9b8b85f0
Leon Ding <deen.job@qq.com>
2023-06-13 23:06:58 +0800

    add: generator yield.

---
+++ chpater11/generator_try.js
@@ -0,0 +1,45 @@
+// js 中的 yield 关键字的 try 和 finally 语句使用，
+// throw 一样可以被使用，用了处理异常错误情况。
+
+// 在自定义可迭代对象中可以复写 return 方法，
+// 但是在 yield 生成器中是无法使用 return 方法，
+// 但是可以在 yield 语句中使用 try catch 语句处理。
+
+// 生成器通过 yield 返回值
+// 调用者通过 next 方法参数传值
+
+function* smallNumbers() {
+    console.log("next() 第 1 次参数会被丢弃.");
+    let y1 = yield 1;
+    console.log("next() 第 2 次参数是 y1 值.", y1);
+    let y2 = yield 2;
+    console.log("next() 第 3 次参数是 y2 值.", y2);
+    let y3 = yield 3;
+    console.log("next() 第 4 次参数是 y2 值.", y3);
+    return 4;
+}
+
+// 每取出一个元素，yield 都会控制代码停止，
+// 直到下一个 next 被取值才执行。
+let g = smallNumbers();
+
+// a 参数被丢弃了
+let n1 = g.next("a");
+
+console.log(n1);
+
+let n2 = g.next("b");
+
+console.log(n2);
+
+let n3 = g.next("c");
+
+console.log(n3);
+
+let n4 = g.next("d");
+
+console.log(n4);
+
+let n5 = g.next("e");
+
+console.log(n5);
