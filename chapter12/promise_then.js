ca0734818719ad41578d9e90d997ae34e8e1d8a2
Leon Ding <deen.job@qq.com>
2023-06-16 13:39:24 +0800

    add: promise es6.

---
+++ chapter12/promise_then.js
@@ -0,0 +1,18 @@
+// js 中的 promise 错误处理教程
+
+// 常见一个 Promise 异步调用任务
+const myPromise = new Promise(
+    // 这两个参数为函数
+    (resolve, reject) => {
+        setTimeout(resolve("execute success."), 1000);
+        setTimeout(reject("execute failure."), 1000);
+    }
+);
+
+myPromise.then(
+    (value) => { throw new Error("runing expiation.") },
+    // 如果不加 catch 这个 error 也是无法处理的
+    (error) => console.log(error),
+).catch(error => {
+    console.log("catch error message:",error)
+});
