ad62670474e127ccdbd88c768af1beedff23a539
Leon Ding <deen.job@qq.com>
2023-06-16 13:57:52 +0800

    add: promise es6.

--- chapter12/promise_then.js
+++ chapter12/promise_then.js
@@ -1,18 +1,20 @@
 // js 中的 promise 错误处理教程
 
 // 常见一个 Promise 异步调用任务
-const myPromise = new Promise(
-    // 这两个参数为函数
-    (resolve, reject) => {
-        setTimeout(resolve("execute success."), 1000);
-        setTimeout(reject("execute failure."), 1000);
+const myPromise = new Promise((resolve, reject) => {
+    const randomNumber = Math.random();
+    if (randomNumber > 0.5) {
+        resolve(randomNumber);
+    } else {
+        // 演示计算出现错误的过程
+        reject(new Error("Random number is less than or equal to 0.5"));
     }
-);
-
-myPromise.then(
-    (value) => { throw new Error(value) },
-    // 如果不加 catch 这个 error 也是无法处理的
-    (error) => console.log(error),
-).catch(error => {
-    console.log("catch error message:", error)
 });
+
+myPromise
+    .then((value) => {
+        console.log("Resolved with value:", value);
+    })
+    .catch((error) => {
+        console.log("Caught an error:", error.message);
+    });
