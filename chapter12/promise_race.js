78fb8beb47245698ded55e9bb1c99caa727d8764
Leon Ding <deen.job@qq.com>
2023-06-16 18:03:45 +0800

    add: promise es6 promise race.

--- chapter12/promise_race.js
+++ chapter12/promise_race.js
@@ -2,23 +2,23 @@
 // promise.race 可以获取最新完成任务哪一个。
 
 const p1 = new Promise((resolve, _) => {
-    let rand = Math.random() * 100;
+    let rand = Math.random() * 1000;
     setTimeout(() => resolve("one"), rand);
 });
 
 const p2 = new Promise((resolve, _) => {
-    let rand = Math.random() * 100;
+    let rand = Math.random() * 1000;
     setTimeout(() => resolve("two"), rand);
 });
 
 const p3 = new Promise((resolve, _) => {
-    let rand = Math.random() * 100;
+    let rand = Math.random() * 1000;
     setTimeout(() => resolve("three"), rand);
 });
 
 Promise.race([p1, p2, p3])
     .then((values) => {
-        console.log("最新完成任务的：",values);
+        console.log("最先完成任务的：",values);
     })
     .catch((error) => {
         console.error(error.message);
