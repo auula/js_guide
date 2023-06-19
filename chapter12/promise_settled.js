8b198d4ba6ff3fdd1d481356d0095d17362d9703
Leon Ding <deen.job@qq.com>
2023-06-19 12:34:25 +0800

    add: promise all settled.

--- chapter12/promise_settled.js
+++ chapter12/promise_settled.js
@@ -8,14 +8,27 @@ let ps = [
     Promise.resolve(2),
     Promise.reject(4),
     Promise.resolve(5),
+    new Promise((resolve, reject) => {
+        const randomNumber = Math.random();
+        if (randomNumber > 0.5) {
+            resolve(randomNumber);
+        } else {
+            // 演示计算出现错误的过程
+            reject("Random number is less than or equal to 0.5");
+        }
+    }),
 ];
 
+// Promise.allSettled 接受一个数组，并且处理每个 Promise 元素
+// Promise 会有两种状态 fulfilled 和 rejected 状态
+// 如果是 fulfilled 状态会绑定一个 value 值
+// 如果是 rejected 状态会绑定一个 reason 值
 Promise.allSettled(ps).then(results => {
     results.forEach(res => {
         if (res.status === 'fulfilled') {
             console.log('Fulfilled:', res.value);
-          } else if (res.status === 'rejected') {
+        } else if (res.status === 'rejected') {
             console.log('Rejected:', res.reason);
-          }
+        }
     });
 })
