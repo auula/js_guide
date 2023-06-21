cdead129bc30b253a1c1f45634e31cfad93a3e50
Leon Ding <deen.job@qq.com>
2023-06-21 23:24:13 +0800

    add: object descriptor.

---
+++ chapter13/descriptor.js
@@ -0,0 +1,32 @@
+// js 中对象数据属性和访问器属性元编程
+// 用代码去操作代码
+
+let obj = {
+    x: 10,
+}
+
+// 获取对象的某一个属性的描述符
+let ds = Object.getOwnPropertyDescriptor(obj,"x");
+
+// { value: 10, writable: true, enumerable: true, configurable: true }
+console.log(ds);
+
+
+// 这个对象有一个只读的访问属性
+const random = {
+    get octet() {
+        return Math.floor(Math.random * 256);
+    }
+}
+
+let ds2 = Object.getOwnPropertyDescriptor(random,"octet");
+
+// {
+//     get: [Function: get octet],
+//     set: undefined,
+//     enumerable: true,
+//     configurable: true
+// }
+
+console.log(ds2);
+
