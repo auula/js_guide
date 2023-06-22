f77c4a09c6823d72c0d767ecd86022e6e2bcc7f8
Leon Ding <deen.job@qq.com>
2023-06-22 16:56:21 +0800

    add: object descriptor configurable.

--- chapter13/configurable.js
+++ chapter13/configurable.js
@@ -1,10 +1,10 @@
 // js 中对象属性的可配置和可写冲突
 
 
-let obj = Object.defineProperty({},"x",{
-    writable:false,
-    configurable:true,
-    value:10,
+let obj = Object.defineProperty({}, "x", {
+    writable: false,
+    configurable: true,
+    value: 10,
 });
 
 // 10
@@ -17,7 +17,7 @@ obj.x = 100;
 console.log(obj.x);
 
 // 因为初次创建的 obj x 属性配置的是可配置
-Object.defineProperty(obj,"x",{
+Object.defineProperty(obj, "x", {
     value: 200,
 })
 
