e239244f0d1f255cbc7155890ed218a11e62915a
Leon Ding <deen.job@qq.com>
2023-06-23 00:47:34 +0800

    add: object extensible.

--- chapter13/extensible.js
+++ chapter13/extensible.js
@@ -16,3 +16,29 @@ obj.y = 200;
 
 // undefined
 console.log(obj.y);
+
+
+// 原型对象
+let prevent = { x: 10 };
+
+// 从原型对象上再创建一个子对象
+let child = Object.create(prevent);
+
+// 设置子对象不可扩展属性
+Object.preventExtensions(child);
+
+console.log(child.x);
+
+// 因为 preventExtensions 设置不可扩展所以这里不能正常设置
+child["y"] = 200;
+
+// undefined
+console.log(child.y);
+
+// preventExtensions 不能限制对象的原型对象的扩展属性
+prevent.y = 200;
+
+// 此时这个子对象就可以访问到 y 属性了 200
+console.log(child.y);
+
+
