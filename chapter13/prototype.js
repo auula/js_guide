753ebbf49a86ef467d48f499564f318375f03218
Leon Ding <deen.job@qq.com>
2023-06-23 20:51:24 +0800

    add: object seal & freeze.

--- chapter13/prototype.js
+++ chapter13/prototype.js
@@ -11,3 +11,15 @@ let o = Object.create(p);
 
 // true p 对象是否为 o 的原型
 console.log(p.isPrototypeOf(o));
+
+
+let arr = [1,2,3,4,5];
+
+// 1,2,3,4,5
+console.log(arr.join());
+
+// 把 arr 原型设置为 p ，此时就会失去默认的 join 方法
+Object.setPrototypeOf(arr,p);
+
+// undefined 运行时异常
+// console.log(arr.join());
