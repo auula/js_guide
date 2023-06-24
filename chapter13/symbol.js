167c759ca97d87bb2d00afe0790d906fdc71d4f0
Leon Ding <deen.job@qq.com>
2023-06-24 16:17:38 +0800

    add: symbol well-known instanceof.

--- chapter13/symbol.js
+++ chapter13/symbol.js
@@ -3,3 +3,17 @@
 
 // 前面的例子中 Symbol.iterator 和 Symbol.asyncIterator 来实现的对象可迭代
 
+// Symbol.hasInstance 新加的公认符号属性，每个对象都可以使用它，作用于 instanceof 表达式右值
+
+let uint8 = {
+    [Symbol.hasInstance](x) {
+        return this.isUint8(x);
+    },
+    
+    isUint8: function (x) {
+        // uint8 范围
+        return Number.isInteger(x) && x >= 0 && x <= 255;
+    },
+};
+
+console.log(uint8.isUint8(128) === 128 instanceof uint8);
