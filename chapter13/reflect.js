54c7a2a29e0a2fd43ca004cb85b3d201e0017d8b
Leon Ding <deen.job@qq.com>
2023-06-26 15:23:57 +0800

    add: symbol well-known to reflect.

--- chapter13/reflect.js
+++ chapter13/reflect.js
@@ -1,4 +1,12 @@
 // js 中的反射特性使用，Reflect 对象提供一系列的关于反射的 API 函数。
 
 
-Reflect.apply();
+let obj = {
+    name : "Leon",
+}
+
+function fun(params) {
+    console.log(params);
+}
+
+Reflect.apply(fun,obj,"Leon");
