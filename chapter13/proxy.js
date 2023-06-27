6e08b56460d7cf44a6e7e081bf3f1740942c84c8
Leon Ding <deen.job@qq.com>
2023-06-27 17:15:28 +0800

    add: symbol well-known to proxy.

--- chapter13/proxy.js
+++ chapter13/proxy.js
@@ -23,13 +23,23 @@ let p = new Person();
 
 // 通过代理来实现某个类型的 say 方法
 let pproxy = new Proxy(p, {
+    // 在代理对象里面设置两个控制属性方法
     get: function (obj, prop) {
         if (prop === "say") {
             return `hi my name is ${obj.name} age ${obj.age}.`;
         }
-    }
+    },
+    set: function (obj,prop,args) {
+        if (prop === "name") {
+            obj.name = args;
+        }
+    },
 });
 
+// 通过代理对象来设置某个属性
+pproxy.name = "Leon Ding";
+
+// hi my name is Leon Ding age 24.
 console.log(pproxy.say);
 
 
