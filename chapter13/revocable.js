cd515f24eedd393c0bcc08f09d85ff5607371efd
Leon Ding <deen.job@qq.com>
2023-06-27 14:11:33 +0800

    add: symbol well-known to proxy.revocable.

--- chapter13/revocable.js
+++ chapter13/revocable.js
@@ -1,9 +1,17 @@
-function accessTheDatabase() { 
+// js 代理中 Proxy.revocable 只不过它除了创建代理对象之外，
+// 还创建一个revoke可以调用来禁用代理的函数。
+// 代理对象和revoke函数包装在一个普通对象中。
+
+function accessTheDatabase() {
     return 42;
 }
 
 // proxy 可以用来操作被代理的对象
-let { proxy, revoke } = Proxy.revocable(accessTheDatabase, {});
+let { proxy, revoke } = Proxy.revocable(accessTheDatabase, {
+    get(o, name, target) {
+        return name;
+    },
+});
 
 // 42
 console.log(proxy());
