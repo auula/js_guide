2596ff86b16c073299b0e0d9024ce9bfd9e5cb1a
Leon Ding <deen.job@qq.com>
2023-06-28 20:36:57 +0800

    add: html structured & js & css.

---
+++ chapter14/basic/async.js
@@ -0,0 +1,18 @@
+// 异步执行脚本可以加快页面的加载速度，特别适用于那些不依赖于其他脚本的独立脚本。
+// 当使用async属性时，脚本会异步加载并立即执行，不会阻塞HTML文档的解析。
+// 多个带有async属性的脚本之间的执行顺序是不确定的，哪个脚本先加载完成就先执行哪个。
+
+// const ap = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("async.js 文件嵌入到 HTML 中的 JS 异步执行了。")
+//     }, 2000);
+// });
+
+// ap.then(res => {
+//     console.log(res);
+// }).catch(err => {
+//     console.error(err);
+// });
+
+console.log("async.js 文件嵌入到 HTML 中的 JS 异步执行了。");
+
---
+++ chapter14/basic/defer.js
@@ -0,0 +1,18 @@
+// 当使用defer属性时，脚本会被延迟执行，直到HTML文档完全被解析和显示之后再执行。
+// 多个带有defer属性的脚本会按照它们在文档中出现的顺序依次执行。
+// 延迟执行脚本可以在文档解析过程中进行其他操作，然后再执行脚本，这有助于提高页面的加载性能。
+
+
+// const dp = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("defer.js 总是最后一次执行的。")
+//     }, 2000);
+// });
+
+// dp.then(res => {
+//     console.log(res);
+// }).catch(err => {
+//     console.error(err);
+// });
+
+console.log("defer.js 总是最后一次执行的。");
