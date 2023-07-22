1f272df19c6339366c6e6882c76db9ed90f761ee
Leon Ding <ding@ibyte.me>
2023-07-23 00:47:05 +0800

    add: client storage.

--- chapter08/class_static.js
+++ chapter08/class_static.js
@@ -6,19 +6,19 @@ class Person {
     firstName = "";
     lastName = "";
 
-    constructor(fname,lname) {
+    constructor(fname, lname) {
         this.firstName = fname;
-        this.lastName  = lname;
+        this.lastName = lname;
     }
 
     // 静态属性构造函数，使用 static 关键字声明的属性为静态
     // 非 static 声明的属性，属于对象实例属性
-    static create([fn = "Leon",ln = "Ding"]) {
-        return new Person(fn,ln);
+    static create([fn = "Leon", ln = "Ding"]) {
+        return new Person(fn, ln);
     }
 }
 
-let name = ["John",,] ?? "Leon Ding";
+let name = ["John", ,] ?? "Leon Ding";
 
 // 静态构造方法
 let p = Person.create(name);
