392dad733ac114de559be695b93f0a657f82670b
Leon Ding <ding@ibyte.me>
2023-08-03 22:33:32 +0800

    add: map structure.

--- chapter10/map.js
+++ chapter10/map.js
@@ -2,10 +2,10 @@
 
 let m = new Map();
 
-m.set("k1",1);
-m.set("k2",2);
-m.set("k3",3);
-m.set("k4",4);
+m.set("k1", 1);
+m.set("k2", 2);
+m.set("k3", 3);
+m.set("k4", 4);
 
 console.log(m.has("k4"));
 
@@ -14,8 +14,8 @@ m.delete("k4");
 console.log(m.has("k4"));
 
 let n = new Map([
-    ["one","1"],
-    ["two","2"],
+    ["one", "1"],
+    ["two", "2"],
 ]);
 
 n.forEach(v => {
@@ -23,13 +23,13 @@ n.forEach(v => {
 });
 
 // 通过解构的方式来实现遍历
-for (const [k,v] of n.entries()) {
-    console.log(k,v);
+for (const [k, v] of n.entries()) {
+    console.log(k, v);
 };
 
 let map = new Map([
-    ["one","1"],
-    ["two","2"],
+    ["one", "1"],
+    ["two", "2"],
 ]);
 
 // 获取 map 的大小
@@ -42,12 +42,12 @@ console.log(map.get("two"));
 // js 作为一种没有严格编程限制的语言，在 Map 类型中可以使用 null 、undefind 、NaN 类型作为键
 
 let sm = new Map([
-    [{},1],
-    [{},2],
-    [{},3],
+    [{}, 1],
+    [{}, 2],
+    [{}, 3],
 ]);
 
-sm.set(sm,undefined);
+sm.set(sm, undefined);
 
 console.log(sm);
 
@@ -59,11 +59,11 @@ console.log(sm);
 
 
 // Map 是可以迭代的，每个元素都是一个元素位为 2 位的数组
-let m2 = new Map([["x",1],["y",2]]);
+let m2 = new Map([["x", 1], ["y", 2]]);
 
 // 可迭代 Map
-for (const [key,value] of m2) {
-    console.log(key,value);
+for (const [key, value] of m2) {
+    console.log(key, value);
 }
 
 // 键名
