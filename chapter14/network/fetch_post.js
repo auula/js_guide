ead13447cf863bb1b01262a568d31af0dec85e4a
Leon Ding <ding@ibyte.me>
2023-08-29 16:08:09 +0800

    add: fetch api network headers.

--- chapter14/network/fetch_post.js
+++ chapter14/network/fetch_post.js
@@ -3,21 +3,24 @@
 // 一个 echo 服务器地址
 const url = "https://httpbin.org/post";
 
-
 // 构建请求体数据
 const data = {
     account: 'user001',
     password: 'xdshdiashdhasd'
 };
 
+// 创建请求头 Headers
+const headers = new Headers();
+
+// 设置请求头信息
+headers.set('Content-Type', 'application/json');
+
 // 构建请求配置对象
 const requestOptions = {
     // 请求类型
     method: 'POST',
     // 设置请求头部为 JSON 格式
-    headers: {
-        'Content-Type': 'application/json'
-    },
+    headers: headers,
     // 将数据转换为 JSON 字符串并作为请求体
     body: JSON.stringify(data)
 };
