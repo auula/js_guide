2f090aa00659083ff9a335785f8b8831fb8a949d
Leon Ding <ding@ibyte.me>
2023-08-29 14:48:46 +0800

    add: fetch api network headers.

--- chapter14/network/fetch_headers.js
+++ chapter14/network/fetch_headers.js
@@ -2,8 +2,24 @@
 const url = "https://randomuser.me/api/";
 
 fetch(url)
-    .then((response) => response.json())
-    .then((responseData) => {
-        console.log(responseData);
-    }
-    );
+    .then(response => {
+        // 通过 response 分析请求状态
+        if (!(response.ok && response.status === 200)) {
+            throw new Error("http error: " + response.status);
+        }
+        return response;
+    })
+    .then(response => {
+        // 遍历输出 response 请求头
+        for (const [key, value] of response.headers) {
+            console.log(`${key}: ${value}`);
+        }
+        return response;
+    })
+    // 解析为 json 数据
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    // 捕获错误
+    .catch(error => console.log(error));
