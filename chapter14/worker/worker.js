1a54e79a65bc4cf5b38608a786d7451840991248
Leon Ding <ding_ms@outlook.com>
2026-05-13 19:01:25 +0800

    add: document thread worker example.

--- chapter14/worker/worker.js
+++ chapter14/worker/worker.js
@@ -1,6 +1,7 @@
 console.log("Worker is running...");
 
-// self 是指 worker 线程本身
+// self 是指 worker 线程本身，WorkerGroupScope 是一个全局对象，提供了与主线程通信的方法和属性。
+// 我们可以通过 self.onmessage 来监听主线程发送的消息，并通过 self.postMessage 来向主线程发送消息。
 self.onmessage = (event) => {
     console.log("Received message from main thread:", event.data);
     // 根据消息类型执行不同的计算任务
