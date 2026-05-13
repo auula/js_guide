0f7a54fa4d90148720765bda46343891050760b1
Leon Ding <ding_ms@outlook.com>
2026-05-13 18:49:26 +0800

    add: document thread worker example.

--- chapter14/worker/worker.js
+++ chapter14/worker/worker.js
@@ -3,8 +3,15 @@ console.log("Worker is running...");
 // self 是指 worker 线程本身
 self.onmessage = (event) => {
     console.log("Received message from main thread:", event.data);
-    // 简单的计算任务
-    const result = event.data * 2;
-    // 将结果发送回主线程
-    self.postMessage(result); 
+    // 根据消息类型执行不同的计算任务
+    switch (event.data.type) {
+        case "compute":
+            // 简单的计算任务
+            const result = event.data.payload * 2;
+            // 将结果发送回主线程
+            self.postMessage(result);
+            break;
+        default:
+            console.error("Unknown message type:", event.data.type);
+    }
 }
