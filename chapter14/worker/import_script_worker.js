7dd40b57194e7fb0e6bbb6ad6ab145e8d72acf70
Leon Ding <ding_ms@outlook.com>
2026-05-13 19:47:32 +0800

    add: web worker importScripts example.

--- chapter14/worker/import_script_worker.js
+++ chapter14/worker/import_script_worker.js
@@ -4,7 +4,6 @@ importScripts('./math.js');
 self.onmessage = function (event) {
     console.log("Received message from main thread:", event.data);
     // 调用 math.js 中的 multiply 函数进行计算
-    const result = multiply(event.data.a, event.data.b);
-    self.postMessage(result);
+    self.postMessage(multiply(event.data.a, event.data.b));
     self.close();
 }
